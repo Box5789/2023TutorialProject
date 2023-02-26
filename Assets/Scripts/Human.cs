21eab15687944dbc23c5f691ed52f6a9466b73fa
Tastypotato0225 <63251068+Tastypotato0225@users.noreply.github.com>
2023-02-27 02:41:28 +0900

    Human_Move

--- Assets/Scripts/Human.cs
+++ Assets/Scripts/Human.cs
@@ -1,8 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class Human : MonoBehaviour
 {
-    
+    [SerializeField] private Vector2 dir;
+    [SerializeField] private bool canMove = false;
+    [SerializeField] private float speed = 2.0f;
+
+    [SerializeField] private Rigidbody2D _rigid;
+
+    private void OnEnable()
+    {
+        Invoke("SetDirection", 1.0f);
+        Invoke("Destroy_Self", 300.0f);
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if(canMove)
+            _rigid.MovePosition(_rigid.position + dir * speed * Time.fixedDeltaTime);
+    }
+
+    private void SetDirection()
+    {
+        canMove = true;
+
+        int r = Random.Range(0,2);
+
+        if (r == 0)
+            dir = Vector2.right;
+        else
+            dir = Vector2.left;
+
+        Invoke("CanMove", 5.0f);
+    }
+
+    private void CanMove()
+    {
+        canMove = false;
+
+        Invoke("SetDirection", 1.5f);
+    }
+
+
+    private void Destroy_Self()
+    {
+        Destroy(gameObject);
+    }
 }
